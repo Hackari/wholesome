d640795b29db37fc530701534afcf70e457d86ba
alieron <sebastien.leib1@gmail.com>
2025-05-17 03:30:25 +0800

    Implemented functionality to handle multiple Game instances simultaneously

--- Game.js
+++ Game.js
@@ -1,4 +1,4 @@
-const Card = require('./Card'); 
+const Card = require('./Card');
 const DeckInit = require('./DeckInit');
 const Player = require('./Player');
 
@@ -20,101 +20,119 @@ const ROYAL_FLUSH = 4;
 const THREE_DIAMONDS = 0;
 
 class Game {
-    constructor(lobby, bot) {
-        this.players = [];
-        this.inversePlayers = [];
-        this.playerCount = 0;
-        this.turn = MAX_PLAYERS;
-        this.currRoundType = ANY;
-        this.currSetType = STRAIGHT;
-        this.high = new Card(THREE_DIAMONDS);
-        this.gameActive = false;
+	static instances = [];
 
-        this.lobby = lobby;
-        this.bot = bot;
+	constructor(chatId, bot) {
+		this.players = [];
+		this.inversePlayers = [];
+		this.playerCount = 0;
+		this.turn = MAX_PLAYERS;
+		this.currRoundType = ANY;
+		this.currSetType = STRAIGHT;
+		this.high = new Card(THREE_DIAMONDS);
+		this.gameActive = false;
 
-        this.deckInit = new DeckInit();
-        this.deckInit.shuffle();
-        console.log(`Created a new game for ${MAX_PLAYERS} players.`);
-    }
+		this.chatId = chatId;
+		this.bot = bot;
 
-    message(userId, text) {
-        this.bot.sendMessage(userId, text);
-    }
+		this.deckInit = new DeckInit();
+		this.deckInit.shuffle();
+		console.log(`Created a new game for ${MAX_PLAYERS} players in ${this.chatId.toString(16)}.`);
 
-    broadcast(text) {
-        for (let i = 0; i < this.playerCount; i++) {
-            const player = this.players[i];
-            this.message(player.userId, text);
-        }
-        this.message(this.lobby, text);
-    }
+		Game.instances.push(this); // WARNING: may lead to memory leaks if instances are created and never destroyed
+	}
 
-    join(usr) {
-        const newPlayer = new Player(usr, this.playerCount, this.deckInit);
-        this.players[this.playerCount] = newPlayer;
-        this.inversePlayers[this.playerCount] = usr.id;
+	destroy() { // call destroy on game end
+		let i = 0;
+		while (Game.instances[i] !== this) { i++; }
+		Game.instances.splice(i, 1);
+	}
 
-        if (newPlayer.first) {
-            this.turn = this.playerCount;
-            console.log(`Three diamonds found.`);
-        }
+	static getGameByChatId(chatId) {
+		return Game.instances.filter(g => g.chatId == chatId)[0];
+	}
 
-        this.playerCount++;
+	message(userId, text) {
+		this.bot.sendMessage(userId, text);
+	}
 
-        let playerJoinMsg = `${newPlayer.username} joined as player ${this.playerCount}`
-        console.log(playerJoinMsg);
-        this.broadcast(playerJoinMsg);
+	broadcast(text) {
+		for (let i = 0; i < this.playerCount; i++) {
+			const player = this.players[i];
+			this.message(player.userId, text);
+		}
+		this.message(this.chatId, text);
+	}
 
-        if (this.playerCount >= MAX_PLAYERS) {
-            this.broadcast(`All players found. Starting game.`)
-        }
-    }
+	gameIsFull() {
+		return this.playerCount >= MAX_PLAYERS;
+	}
 
-    getPlayer(usr) {
-        const player = this.players[this.inversePlayers.indexOf(usr.id)];
-        return player;
-    }
+	join(usr) {
+		const newPlayer = new Player(usr, this.playerCount, this.deckInit);
+		this.players[this.playerCount] = newPlayer;
+		this.inversePlayers[this.playerCount] = usr.id;
 
-    playerAction(usr, action) {
-        const player = this.getPlayer(usr);
-        const resultMsg = action(player)
-        this.message(player.userId, resultMsg);
-    }
+		if (newPlayer.first) {
+			this.turn = this.playerCount;
+			console.log(`Three diamonds found.`);
+		}
 
-    showHand(usr) {
-        this.playerAction(usr, 
-            (player) => player.showHand()
-        );
-    }
+		this.playerCount++;
 
-    sortHand(usr) {
-        this.playerAction(usr, 
-            (player) => player.sortHand()
-        );
-    }
+		let playerJoinMsg = `${newPlayer.username} joined as player ${this.playerCount}`
+		console.log(playerJoinMsg);
+		// this.broadcast(playerJoinMsg);
 
-    showStatus(usr) {
-        const player = this.getPlayer(usr);
-        let statusMsg = `Total players: ${this.playerCount}`
-        statusMsg += `Current Round Type: ${ROUND_TYPES[this.currRoundType]}\n`;
-        for (let i = 0; i < this.playerCount; i++) {
-            const player = this.players[i];
-            const playerMsg = player.getStatus();
-            statusMsg += playerMsg + "\n";
-        }      
-        this.message(player.userId, statusMsg); 
-    }
+		// if (this.gameIsFull()) {
+		//	this.broadcast(`All players found. Starting game.`)
+		// }
+	}
 
-    play(usr, text) {
-        const cardIndices = text.split(' ').slice(1);
-        this.playerAction(usr,
-            (player) => player.playCards(cardIndices, 
-                this.currRoundType, 
-                this.currSetType, 
-                this.high)
-        )
-    }
+	getPlayer(usr) {
+		const player = this.players[this.inversePlayers.indexOf(usr.id)];
+		return player;
+	}
+
+	playerAction(usr, action) {
+		const player = this.getPlayer(usr);
+		const resultMsg = action(player)
+		this.message(player.userId, resultMsg);
+	}
+
+	showHand(usr) {
+		this.playerAction(usr,
+			(player) => player.showHand()
+		);
+	}
+
+	sortHand(usr) {
+		this.playerAction(usr,
+			(player) => player.sortHand()
+		);
+	}
+
+	showStatus(usr) {
+		const player = this.getPlayer(usr);
+		let statusMsg = `Total players: ${this.playerCount}`
+		statusMsg += `Current Round Type: ${ROUND_TYPES[this.currRoundType]}\n`;
+		for (let i = 0; i < this.playerCount; i++) {
+			const player = this.players[i];
+			const playerMsg = player.getStatus();
+			statusMsg += playerMsg + "\n";
+		}
+		this.message(player.userId, statusMsg);
+	}
+
+	play(usr, text) {
+		const cardIndices = text.split(' ').slice(1);
+		this.playerAction(usr,
+			(player) => player.playCards(cardIndices,
+				this.currRoundType,
+				this.currSetType,
+				this.high)
+		)
+	}
 
 }
 
--- bot.js
+++ bot.js
@@ -4,14 +4,20 @@ const Game = require('./Game.js');
 
 const bot = new TelegramBot(config.token, { polling: true });
 
-let game = 0;
 
 // Start
 bot.onText(/\/start/, (msg) => {
-	const lobby = msg.chat.id;
-	game = new Game(lobby, bot);
-	bot.sendMessage(lobby, `Game started!`, {
-		reply_markup: { inline_keyboard: [[{ text: 'Join', callback_data: 'join_game' }]] }
+	const chatID = msg.chat.id;
+	if (msg.chat.type == 'private' || msg.chat.type == 'channel') {
+		bot.sendMessage(chatID, `Start the game in a group chat`);
+		return;
+	}
+	const game = new Game(chatID, bot);
+	bot.sendMessage(chatID, `Game started! 1/4\n- ${msg.from.username}`, {
+		reply_markup: {
+			inline_keyboard: [
+				[{ text: 'Join', callback_data: 'join_game' }]]
+		}
 	});
 	game.join(msg.from);
 });
@@ -19,26 +25,35 @@ bot.onText(/\/start/, (msg) => {
 // Handle callbacks
 bot.on('callback_query', (query) => {
 	const callback_data = query.data;
+	const msg = query.message;
+	const game = Game.getGameByChatId(msg.chat.id);
 	if (callback_data == "join_game") {
-		game.join(query.from)
+		game.join(query.from);
+		const opts = { chat_id: msg.chat.id, message_id: msg.message_id };
+		if (game.gameIsFull()) {
+			bot.editMessageText(`All players found.\nStarting game.`, opts);
+			bot.editMessageReplyMarkup({}, opts);
+		} else {
+			bot.editMessage(`${msg.text.substring(0, 14)}${game.playerCount}${msg.text.substring(14 + 1)}\n- ${query.from.username}`, opts);
+		}
 	}
 });
 
 bot.onText(/\/join/, (msg) => {
-	game.join(msg.from);
-})
+	Game.getGameByChatId(msg.chat.id).join(msg.from);
+});
 
 bot.onText(/hand/, (msg) => {
-	game.showHand(msg.from);
-})
+	Game.getGameByChatId(msg.chat.id).showHand(msg.from);
+});
 
 bot.onText(/sort/, (msg) => {
-	game.sortHand(msg.from);
-})
+	Game.getGameByChatId(msg.chat.id).sortHand(msg.from);
+});
 
 bot.onText(/status/, (msg) => {
-	game.showStatus(msg.from);
-})
+	Game.getGameByChatId(msg.chat.id).showStatus(msg.from);
+});
 
 // function startTurn(newTurn) {
 //   const playerId = playerLookup[turn];
